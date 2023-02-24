fdfad84ed2461c2ac72d9e28bb237d6bb1097dda
Rihanul Islam <rihanthedev@gmail.com>
2023-02-24 19:02:28 +0600

    products show

--- js/meal.js
+++ js/meal.js
@@ -1,12 +1,40 @@
-const loadMeals=()=>{
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
+const loadMeals=(searchText)=>{
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    fetch(url)
     .then(res=> res.json())
     .then(res => displayMeals(res.meals))
 }
 
 const displayMeals = meals =>{
     // console.log(meals);
-    
+    const mealsContainer = document.getElementById('mealContainer')
+    mealsContainer.innerHTML = ''
+    meals.forEach(meal => {
+        // console.log(meal);
+        const mealsDiv = document.createElement('div')
+        mealsDiv.classList.add('col')
+        mealsDiv.innerHTML = `
+        <div class="card h-100">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+        </div>
+        `
+        mealsContainer.appendChild(mealsDiv);
+    });
 }
 
-loadMeals()
+const searchMeal = ()=>{
+    const searchTextElement = document.getElementById('search-filed');
+    const searchText = searchTextElement.value ;
+    searchTextElement.value = '';
+    loadMeals(searchText);
+    
+    
+    // console.log(searchText)
+    
+    
+}
+loadMeals('fish')
