1a64cc62f4cb1efb933a632e534343ad65d69043
Rihanul Islam <rihanthedev@gmail.com>
2023-02-24 15:54:22 +0600

    bidehini

--- js/random.js
+++ js/random.js
@@ -1,4 +1,20 @@
-/*
- you need to show random user form api.
- thankYou. goodNight
-**/ 
+const loadUser=()=>{
+    fetch('https://randomuser.me/api/')
+    .then(res=>res.json())
+    .then(data => displayData(data))
+}
+function displayData(user){
+    const genderElement = document.getElementById('gender')
+    const nameElement = document.getElementById('name');
+    const titleElement = document.getElementById('title')
+    console.log(user.results[0]);
+    const titles = user.results[0].name.title;
+    const genderData = user.results[0].gender;
+    const fullName = user.results[0].name.first    + ' '+ user.results[0].name.last;
+    
+    console.log(fullName);
+    genderElement.innerText = genderData
+    nameElement.innerText = fullName;
+    titleElement.innerText = titles
+}
+loadUser()
